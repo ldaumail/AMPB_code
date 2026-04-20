1ae3cebe4c23714b8d58d97b4b341d56ebf79b4e
ldaumail <49167117+ldaumail@users.noreply.github.com>
2026-04-20 15:10:13 -0400

    whole brain py

--- 18_whole_brain_participant_pyAFQ_gpu_wmgmi.py
+++ 18_whole_brain_participant_pyAFQ_gpu_wmgmi.py
@@ -13,7 +13,7 @@ BUNDLES_KWARGS = {
 
 def main(dwi_data_file, bval_file, bvec_file, t1_file, output_dir):
   # define custom bundles dictionary
-  bundles = {} # intialize empty bundles dictionary
+  # bundles = {} # intialize empty bundles dictionary
   # hemis = {"L","R"}
   # for hemi in hemis: # for each roi (start/stop) file  
     # define tract label from start and end files 
@@ -29,11 +29,11 @@ def main(dwi_data_file, bval_file, bvec_file, t1_file, output_dir):
     #   # "end": end_file,
     #   **BUNDLES_KWARGS
     # }
-  bundles = {
-    **BUNDLES_KWARGS
-  }
+  # bundles = {
+  #   **BUNDLES_KWARGS
+  # }
   # define bundles as BundleDict
-  bundles = abd.BundleDict(bundles, resample_subject_to=True)
+  # bundles = abd.BundleDict(bundles, resample_subject_to=True)
 
 
 #   scalars = [
@@ -57,9 +57,9 @@ def main(dwi_data_file, bval_file, bvec_file, t1_file, output_dir):
   #"dist_to_atlas": 0, "cleaning_params": {"distance_threshold": 3}
   #"dist_to_atlas" specifies the distance from the target ROIs that tracts need to reach. if = 0, tracts need to reach the surface of ROI, or enter it. If 4 mm = needs to be within 4mm of ROI surface. 
   #"distance_threshold" in cleaning params is the Mahalanobis distance in number of STDEVs. We adjust it to exclude outlier streamlines.
-  segmentation_params = {
-        "cleaning_params": {"distance_threshold": 3, "clean_rounds": 2}
-        } #"dist_to_atlas": 0 
+  # segmentation_params = {
+  #       "cleaning_params": {"distance_threshold": 3, "clean_rounds": 2}
+  #       } #"dist_to_atlas": 0 
        
   # define ParticipantAFQ object
   myafq = ParticipantAFQ(
@@ -68,9 +68,9 @@ def main(dwi_data_file, bval_file, bvec_file, t1_file, output_dir):
     bvec_file             = bvec_file,
     t1_file               = t1_file,
     output_dir            = output_dir,
-    bundle_info           = bundles,
+    # bundle_info           = bundles,
     tracking_params       = tracking_params, 
-    segmentation_params   = segmentation_params,
+    # segmentation_params   = segmentation_params,
     tractography_ngpus    = 1
   )
   
@@ -92,7 +92,6 @@ if __name__ == "__main__":
     bval_file      = args.bval_file, 
     bvec_file      = args.bvec_file,
     t1_file        = args.t1_file,  
-    roi_files      = args.roi_files,
     output_dir     = args.output_dir
   )
 
